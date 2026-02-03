5a8f3a301fbebb1b6dbb031cb8c800c6cca2eb8e
Bruno Molina <145416675+MoBS403@users.noreply.github.com>
2026-02-02 23:55:57 -0300

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -308,11 +308,12 @@ TEMPLATES = [
 
 DATABASES = {
     "default": dj_database_url.config(
-        default="sqlite:///db.sqlite3",
+        default=os.getenv("DATABASE_URL"),
         conn_max_age=600,
         ssl_require=True,
     )
 }
+
 
 
 
